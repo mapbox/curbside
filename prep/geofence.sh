926139de0676db74de91a11986adbdd461f28c30
Chris Toomey <chris.toomey@mapbox.com>
2020-07-28 14:18:38 -0700

    Uncomment prep scripts

--- prep/geofence.sh
+++ prep/geofence.sh
@@ -1,7 +1,7 @@
-# npm run prep
+npm run prep
 sed "s/username/$1/g" ./prep/draft.json | sed "s/tilesetid/$2/g" > ./prep/recipe.json
-# tilesets add-source $1 $2 ./prep/isochrones.geojson 
-# tilesets create $1.$2 --recipe ./prep/recipe.json -n "$2"
-# tilesets publish $1.$2
+tilesets add-source $1 $2 ./prep/isochrones.geojson 
+tilesets create $1.$2 --recipe ./prep/recipe.json -n "$2"
+tilesets publish $1.$2
 
-# echo geofence ID = $1.$2
+echo geofence ID = $1.$2
